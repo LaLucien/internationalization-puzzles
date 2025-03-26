cd9fe0bcd05bf8c11f7c9a15b37d2fac38228459
Lucien <lucien.lang@proton.me>
2025-03-26 20:15:52 +0100

    init repo

---
+++ TEMPLATE/main.py
@@ -0,0 +1,29 @@
+DAY = -1
+FILE = "test.txt"
+
+
+def getInput():
+
+    with open(f"puzzle_{DAY}/{FILE}", "r") as file:
+        pass
+
+    return
+
+
+def solve1(input):
+
+    return
+
+
+def solve2(input):
+
+    return
+
+
+if __name__ == "__main__":
+    input = getInput()
+    sol1 = solve1(input)
+    sol2 = solve2(input)
+
+    print(f"Solution 1: {sol1}")
+    print(f"Solution 2: {sol2}")
