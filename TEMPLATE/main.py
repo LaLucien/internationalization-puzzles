58f2d23b6db82e2c70a0a16860976b40e6a9586e
Lucien <lucien.lang@proton.me>
2025-03-26 20:45:02 +0100

    matched template to puzzle scheme

--- TEMPLATE/main.py
+++ TEMPLATE/main.py
@@ -1,10 +1,11 @@
 DAY = -1
-FILE = "test.txt"
+FILE = "puzzle.txt"
+# FILE = "test.txt"
 
 
 def getInput():
 
-    with open(f"puzzle_{DAY}/{FILE}", "r") as file:
+    with open(f"puzzle_{DAY}/{FILE}", "r", encoding="utf-8") as file:
         pass
 
     return
@@ -15,15 +16,8 @@ def solve1(input):
     return
 
 
-def solve2(input):
-
-    return
-
-
 if __name__ == "__main__":
     input = getInput()
     sol1 = solve1(input)
-    sol2 = solve2(input)
 
     print(f"Solution 1: {sol1}")
-    print(f"Solution 2: {sol2}")
